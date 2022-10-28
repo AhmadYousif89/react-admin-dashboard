aae9837750b7646beaa8e9ea66ecccf6bf1a6029
Ahmad Yousif89 <ayob.4wrk89@gmail.com>
2022-10-29 00:17:09 +0200

    postcss-fix

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,10 +1,3 @@
-require('postcss-custom-properties')({
-  preserve: false, // completely reduce all css vars
-  importFrom: [
-    'src/index.css', // look here for the new values
-  ],
-});
-require('postcss-calc');
 module.exports = {
   plugins: {
     tailwindcss: {},
